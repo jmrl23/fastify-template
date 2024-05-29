5bf29ce10c1e9779fb992e5606788de169f23d12
Jomariel Gaitera <jojogaitera@gmail.com>
2024-05-29 18:37:21 +0800

    Update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import fastify from 'fastify';
 import { NotFound } from 'http-errors';
+import setupPlugin from './plugins/setup.plugin';
 
 const app = fastify();
+
+app.register(setupPlugin);
 
 app.setNotFoundHandler((request) => {
   throw new NotFound(`Cannot ${request.method} ${request.url}`);
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
 import './init';
+import { PORT } from './lib/constants/environment';
 import app from './app';
-import setupPlugin from './plugins/setup.plugin';
-import env from 'env-var';
 import detectPort from 'detect-port';
 import logger from './lib/utils/logger';
 
 async function main() {
-  await app.register(setupPlugin);
+  const port = await detectPort(PORT);
 
-  const port = await detectPort(env.get('PORT').default(3001).asPortNumber());
-
-  app.listen(
-    {
-      port,
-    },
-    function (_error, address) {
-      logger.info(address);
-    },
-  );
+  app.listen({ port }, function (_error, address) {
+    logger.info(address);
+  });
 }
 
 void main();
