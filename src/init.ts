597c220bc25706535cd27d25fc6ff7f349597110
Jomariel Gaitera <gaiterajomariel@gmail.com>
2025-11-13 21:48:26 +0800

    fix: remove environment variables  override

--- src/init.ts
+++ src/init.ts
@@ -12,28 +12,26 @@ declare global {
   }
 }
 
-if (process.env.NODE_ENV === undefined) {
-  process.env.NODE_ENV = 'development';
-}
-
-const ENV_PATHS = glob.globSync(
-  [
-    '.env',
-    `.env.${process.env.NODE_ENV}`,
-    '.env.local',
-    `.env.${process.env.NODE_ENV}.local`,
-  ].map((file) => path.resolve(__dirname, '../', file)),
-  {
-    absolute: true,
-  },
-);
-
 if (workerThreads.isMainThread) {
+  if (process.env.NODE_ENV === undefined) {
+    process.env.NODE_ENV = 'development';
+  }
+
+  const ENV_PATHS = glob.globSync(
+    [
+      `.env.${process.env.NODE_ENV}.local`,
+      '.env.local',
+      `.env.${process.env.NODE_ENV}`,
+      '.env',
+    ].map((file) => path.resolve(__dirname, '../', file)),
+    {
+      absolute: true,
+    },
+  );
+
   dotenvx.config({
     path: ENV_PATHS,
     encoding: 'utf8',
     quiet: process.env.NODE_ENV !== 'development',
-    // Override is true to allow .env files injection precedence
-    override: true,
   });
 }
