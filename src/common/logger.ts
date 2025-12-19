e2021e5e84e11e6f4683a2b6ee2fe42097d5092b
Jomariel Gaitera <gaiterajomariel@gmail.com>
2025-12-19 15:36:18 +0800

    fix: add logger pino pretty transport check for development

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -42,19 +42,20 @@ const baseLoggerOptions: LoggerOptions = {
     NODE_ENV === 'production' ? productionSerializer : developmentSerializer,
 };
 
-const loggerOptions: LoggerOptions =
-  NODE_ENV === 'development'
-    ? {
-        ...baseLoggerOptions,
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-            ignore: 'pid,hostname',
-            translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
-          },
-        },
-      }
-    : baseLoggerOptions;
+const loggerOptions = baseLoggerOptions;
+
+if (NODE_ENV === 'development') {
+  try {
+    require.resolve('pino-pretty');
+    loggerOptions.transport = {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        ignore: 'pid,hostname',
+        translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
+      },
+    };
+  } catch {}
+}
 
 export const logger = pino(loggerOptions);
